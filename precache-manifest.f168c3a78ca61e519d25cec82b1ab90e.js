7d1ef093370763e4e7ea5a1f0e5390f1dbfa801c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-26 23:31:49 +0000

    Deploy ynsem/ynsem.github.io to github.com/ynsem/ynsem.github.io.git:gh-pages

---
+++ precache-manifest.f168c3a78ca61e519d25cec82b1ab90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15c4abd3034a0f19d8e2932a0804b46",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "319096855e5173816bdd",
+    "url": "/todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30f7ca68147c7489002b",
+    "url": "/todo/static/js/2.be571024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.be571024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319096855e5173816bdd",
+    "url": "/todo/static/js/main.a0f755da.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
